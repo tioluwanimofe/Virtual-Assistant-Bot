276fada3266bc60b9056afb1a104a0c82032e15c
Mofe_Sanya <86200511+tioluwanimofe@users.noreply.github.com>
2022-06-20 16:27:03 +0100

    Update VAB.py

--- VAB.py
+++ VAB.py
@@ -1,13 +1,6 @@
-# Description: This is a virtual assistant program that gets the date, current time, responds back with a random
-# greeting , and returns information on a person.
+"""Description: This is a virtual assistant program that gets the date, current time, responds back with a random
+ greeting , and returns information on a person."""
 
-# pip install pyaudio
-# pip install SpeechRecognition
-# pip install gTTs(google text to speed) package
-# pip install wikipedia
-# pip install Google Maps
-# pip install tracker
-# Import the libraries
 import speech_recognition as sr
 import os
 from gtts import gTTS
@@ -16,105 +9,189 @@ import warnings
 import calendar
 import random
 import wikipedia
+import requests
 
-# import googlesearch
-# import tracker
+month_names = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October',
+               'November', 'December']
 
-# Ignore any warning messages
-warnings.filterwarnings('ignore')
+ordinalNumbers = ['1st', '2nd', '3rd', '4th', '5th', '6th', '7th', '8th', '9th', '10th', '11th', '12th', '13th',
+                  '14th', '15th', '16th', '17th', '18th', '19th', '20th', '21st', '22nd', '23rd', '24th',
+                  '25th', '26th', '27th', '28th', '29th', '30th', '31st']  # A list of ordinal numbers
 
-# Record audio and return it as a string
-# Record the audio.
-r = sr.Recognizer()  # Creating a recognizer object
+GREETING_INPUTS = ['hi', 'hey there!!', 'hola', 'greetings', 'was sup', 'hello', 'sup', 'morning', 'afternoon']
 
-# Open the microphone and start recording
-with sr.Microphone() as source:
-    print('Say something pls!!!')
-    audio = r.listen(source)
+GREETING_RESPONSES = ['Howdy', 'Hi, I am VAB', 'Hello', 'Greetings to you to', 'I am good', 'Sup',
+                      'Good morning to you to', 'Good afternoon to you to', ]  # Greeting responses.
 
-# Use Google's speech recognition
 data = ''
 try:
-    data = r.recognize_google(audio)
-    words = ['this is a boy', 'okay vab', 'vab']
-    # A list of wake words
-    # Check to see if the users command/text contains a wakeWord/phrase
+    # while True:
+    warnings.filterwarnings('ignore')  # Ignore any warning messages
+    r = sr.Recognizer()  # Creating a recognizer object
+    with sr.Microphone() as source:  # Open the microphone and start recording
+        print('Say something pls!!!')
+        audio = r.listen(source)
+
+    ''' A function to get the virtual assistant response orally and not written/texted.'''
+    data = r.recognize_google(audio)  # Use Google's speech recognition
     Opemipo = data.split()
-    f = "Yes master, I heard your wake-word"
     mofe = 'You said: ' + data
-    if words in Opemipo:
-        my_obj = gTTS(text=f, lang='en', slow=False)
-        # Save the converted audio to a file
-        my_obj.save('assistant_response.mp3')
+    wakeWord = ['hello', 'boy']  # A list of wake words
+    Supreme = ['mofe', 'muffin']
 
-        # Play the converted file
-        os.system('start assistant_response.mp3')
-
-        # A function to get the virtual assistant response orally and not written/texted.
-        # Convert the text to speech.
-    else:
-        my_obj = gTTS(text=mofe, lang='en', slow=False)
-
-        # Save the converted audio to a file
-        my_obj.save('assistant_response.mp3')
-
-        # Play the converted file
-        os.system('start assistant_response.mp3')
-
-        print(mofe)
-
-
-    # A function to get the current date.
-    def getDate():
-        now = datetime.datetime.now()
+    def getDate():  # A function to get the current date.
+        present = datetime.datetime.now()
         my_date = datetime.datetime.today()
         weekday = calendar.day_name[my_date.weekday()]  # e.g. Friday.
-        monthNum = now.month
-        dayNum = now.dayx
-        # Record the audio
-        text = recordAudio()
-        response = ''
+        monthNum = present.month
+        dayNum = present.day
+        i = 'Today is ' + weekday + ' ' + month_names[monthNum - 1] + ' ' + 'the' + ' ' + ordinalNumbers[dayNum - 1] + \
+            '. '
+        your_date = gTTS(text=i, lang='en', slow=False)  # Convert the text to speech.
+        your_date.save('assistant_date.mp3')  # Save the converted audio to a file
+        os.system('start assistant_date.mp3')  # Play the converted file
+        print(mofe)
+        print("I said: " + i)
 
-        # Check for the wake word/phrase
+
+    def greeting():  # A function to return a random greeting response
+        # Check if the user's input is a greeting, return a randomly chosen to greet response
+        k = random.choice(GREETING_RESPONSES) + '.'
+        your_greeting = gTTS(text=k, lang='en', slow=False)  # Convert the text to speech.
+        your_greeting.save('assistant_greeting.mp3')  # Save the converted audio to a file
+        os.system('start assistant_greeting.mp3')  # Play the converted file
+        print(mofe)
+        print("I said: " + k)
 
 
-    if wakeWord(text):
+    def getPerson():  # A function to get a persons first and last name from the text
+        for i in range(0, len(wordList)):
+            if i + 3 <= len(wordList) - 1 and wordList[i].lower() == 'who' and wordList(i + 1).lower() == 'is':
+                return wordList[i + 2] + ' ' + wordList[i + 3]
 
+
+    if any(Adesanya in Opemipo for Adesanya in wakeWord):
+        my_obj = gTTS(text="Yes master, I heard your wake-word", lang='en', slow=False)  # Convert the text to speech.
+        my_obj.save('assistant_response.mp3')  # Save the converted audio to a file
+        os.system('start assistant_response.mp3')  # Play the converted file
+        teni = 'You said: ' + data
+        print(teni)
+        print("I said: Yes master, I heard your wake-word")
+
+        r = sr.Recognizer()  # Creating a recognizer object
+        with sr.Microphone() as source:  # Open the microphone and start recording
+            print('Say something pls!!!')
+            audio = r.listen(source)
+        data = r.recognize_google(audio)
+        Opemipo = data.split()
         # Check for greetings by the user
-        response = response + greeting(text)
+        while True:
+            if 'date' in Opemipo:  # Check to see if the user said anything having to do with date
+                getDate()
+                break
 
-        # Check to see if the user said anything having to do with date
-        if 'date' in text:
-            getDate()
-            response = response + ' ' + get_date + '.'
+            if any(Adesanya in Opemipo for Adesanya in GREETING_INPUTS):
+                greeting()
+                break
 
-            # Check to see if the user said anything having to do with the time
-        if 'time' in text:
-            now = datetime.datetime.now()
-            meridiem = ''
-            if now.hour >= 12:
-                meridiem = 'p.m'  # Post meridiem (PM) after mid-day
-                hour = now.hour - 12
-            else:
-                meridiem = 'a.m'  # Ante-Meridiem(AM) before mid-day
-                hour = now.hour
+            if 'time' in Opemipo:  # Check to see if the user said anything having to do with the time
+                meridiem = ''
+                now = datetime.datetime.now()
+                if now.hour >= 12:
+                    meridiem = 'p.m'  # Post meridiem (PM) after mid-day
+                    hour = now.hour - 12
+                else:
+                    meridiem = 'a.m'  # Ante-Meridiem(AM) before mid-day
+                    hour = now.hour
 
-                # Convert minute into a proper string.
-            if now.minute < 10:
-                minute = '0' + str(now.minute)
-            else:
-                minute = str(now.minute)
+                if now.minute < 10:
+                    minute = '0' + str(now.minute)  # Convert minute into a proper string.
+                else:
+                    minute = str(now.minute)
+                t = 'It is  ' + str(hour) + ':' + minute + ' ' + meridiem + '.'
+                your_time = gTTS(text=t, lang='en', slow=False)  # Convert the text to speech.
+                your_time.save('assistant_time.mp3')  # Save the converted audio to a file
+                os.system('start assistant_time.mp3')  # Play the converted file
+                print(mofe)
+                print("I said: " + t)
+                break
 
-                response = response + ' ' + 'It is  ' + str(hour) + ':' + minute + ' ' + meridiem + '.'
-            # Check to see if the user said 'who is'.
-        if 'who is' in text:
-            person = getPerson(text)
-            wiki = wikipedia.summary(person, sentences=2)
-            response = response + ' ' + wiki
+            if 'who is' in Opemipo:  # Check to see if the user said 'who is'.
+                person = getPerson()
+                wiki = wikipedia.summary(person, sentences=2)
+                your_person = gTTS(text=wiki, lang='en', slow=False)  # Convert the text to speech.
+                your_person.save('assistant_person.mp3')  # Save the converted audio to a file
+                os.system('start assistant_person.mp3')  # Play the converted file
+                print(mofe)
+                print("I said: " + wiki)
+                break
+
+    elif any(Adesanya in Opemipo for Adesanya in Supreme):
+        Tio = 'Yes Supreme Master, you called the Supreme wake-word, " Adesanya Tioluwanimofe".'
+        my_obj = gTTS(text=Tio, lang='en', slow=False)  # Convert the text to speech.
+        my_obj.save('assistant_response.mp3')  # Save the converted audio to a file
+        os.system('start assistant_response.mp3')  # Play the converted file
+        teni = 'You said: ' + data
+        print(teni)
+        print("I said: Yes Supreme Master, I heard your wake-word")
+        r = sr.Recognizer()  # Creating a recognizer object
+        with sr.Microphone() as source:  # Open the microphone and start recording
+            print('Say something pls!!!')
+            audio = r.listen(source)
+        data = r.recognize_google(audio)
+        Opemipo = data.split()
+        # Check for greetings by the user
+        while True:
+            if 'date' in Opemipo:  # Check to see if the user said anything having to do with date
+                getDate()
+                break
+
+            if any(Adesanya in Opemipo for Adesanya in GREETING_INPUTS):
+                greeting()
+                break
+
+            if 'time' in Opemipo:  # Check to see if the user said anything having to do with the time
+                meridiem = ''
+                now = datetime.datetime.now()
+                if now.hour >= 12:
+                    meridiem = 'p.m'  # Post meridiem (PM) after mid-day
+                    hour = now.hour - 12
+                else:
+                    meridiem = 'a.m'  # Ante-Meridiem(AM) before mid-day
+                    hour = now.hour
+
+                if now.minute < 10:
+                    minute = '0' + str(now.minute)  # Convert minute into a proper string.
+                else:
+                    minute = str(now.minute)
+                t = 'It is  ' + str(hour) + ':' + minute + ' ' + meridiem + '.'
+                your_time = gTTS(text=t, lang='en', slow=False)  # Convert the text to speech.
+                your_time.save('assistant_time.mp3')  # Save the converted audio to a file
+                os.system('start assistant_time.mp3')  # Play the converted file
+                print(mofe)
+                print("I said: " + t)
+                break
+
+            if 'who is' in Opemipo:  # Check to see if the user said 'who is'.
+                person = getPerson()
+                wiki = wikipedia.summary(person, sentences=2)
+                your_person = gTTS(text=wiki, lang='en', slow=False)  # Convert the text to speech.
+                your_person.save('assistant_person.mp3')  # Save the converted audio to a file
+                os.system('start assistant_person.mp3')  # Play the converted file
+                print(mofe)
+                print("I said: " + wiki)
+                break
+
+    else:
+        my_obj = gTTS(text='To access my features, you need to say a wake word', lang='en', slow=False)  # Convert the
+        # text to speech.
+        my_obj.save('assistant_response.mp3')  # Save the converted audio to a file
+        os.system('start assistant_response.mp3')  # Play the converted file
+        print(mofe)
+        print(g)
 
 
-except sr.UnknownValueError:
-    # Check for unknown errors
+except sr.UnknownValueError:  # Check for unknown errors
     print('I do not understand what you said. Please repeat your words slowly again')
 except sr.RequestError as e:
     b = str(e)
